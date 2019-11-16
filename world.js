b6d78e298972d6566a1d7b64d533a37d9e53964e
Jason Williams <williamsjacen007@gmail.com>
2019-11-16 18:15:24 -0500

    Updated files
    Just before part 4

---
+++ world.js
@@ -0,0 +1,25 @@
+function cSearch(){
+    var check = document.getElementById("country").value;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            document.getElementById("result").innerHTML = this.responseText;
+            console.log(this.responseText.replace(/<\/?[^>]+(>|$)/g, " ")); 
+        }
+    };
+    
+    if (document.getElementById('allCheck').checked)
+    {
+        var url = "world.php?all=true";
+    }
+    else
+    {
+        var url = "world.php?country="+check;
+    }
+    
+    
+    request.open("GET",url,true);
+    request.send("");
+}
--- world.php
+++ world.php
@@ -1,17 +1,35 @@
 <?php
+
+
 $host = getenv('IP');
-$username = 'lab7_user';
-$password = '';
+$username = 'Jason7';
+$password = 'professionalism';
 $dbname = 'world';
+$country = $_GET['country'];
+$every = $_GET['all'];
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+if ($every == true && (isset($every) || !empty($all))) 
+{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) 
+    {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
+elseif (isset($country) || !empty($country)) 
+{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) 
+    {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
